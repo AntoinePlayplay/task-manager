a8f323b15cb0e080bd45b9c0301fff98ccbdf6c6
AntoineNguyen <antoine.nguyen@playplay.com>
2023-06-15 00:56:19 +0200

    fix: Handle stamp on query controllers api

--- src/Interface/Api/GetTaskController.php
+++ src/Interface/Api/GetTaskController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class GetTaskController extends AbstractController
@@ -21,7 +22,10 @@ final class GetTaskController extends AbstractController
     {
         $taskQuery = new GetTaskQuery(new UUID($uuid));
         $task = $this->queryBus->dispatch($taskQuery);
+        /** @var HandledStamp $handled */
+        $handled = $task->last(HandledStamp::class);
+        $taskResult = $handled->getResult();
 
-        return $this->json($task, Response::HTTP_OK);
+        return $this->json($taskResult, Response::HTTP_OK);
     }
 }
--- src/Interface/Api/GetTaskListController.php
+++ src/Interface/Api/GetTaskListController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class GetTaskListController extends AbstractController
@@ -19,7 +20,10 @@ final class GetTaskListController extends AbstractController
     public function __invoke(): JsonResponse
     {
         $tasks = $this->queryBus->dispatch(new GetTaskListQuery());
+        /** @var HandledStamp $handled */
+        $handled = $tasks->last(HandledStamp::class);
+        $tasksResult = $handled->getResult();
 
-        return $this->json($tasks, Response::HTTP_OK);
+        return $this->json($tasksResult, Response::HTTP_OK);
     }
 }
